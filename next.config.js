30068a0932f3e953ffeb51d68c8fefe08492176b
Do Young <hello03122@naver.com>
2026-02-02 15:59:35 +0900

    next.config 수정

--- next.config.js
+++ next.config.js
@@ -8,10 +8,17 @@ module.exports = {
 }
 
 /** @type {import('next').NextConfig} */
+const repo = "portfolio-react01"; // 레포 이름
+
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
+
+  // ✅ 프로젝트 페이지면 basePath 필요
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 module.exports = nextConfig;
+
